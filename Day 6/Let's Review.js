667f6bcad273a313176d344baf62526b1157bf51
badshab454 <badshab454@gmail.com>
2022-10-17 21:18:13 +0600

    Let's Review

--- Day 6/Let's Review.js
+++ Day 6/Let's Review.js
@@ -1,5 +1,14 @@
 function processData(input) {
     //Enter your code here
+    const stringArr = input.split('\n');
+    for(let i = 1; i <= parseInt(stringArr[0]); i++ ) {
+        let evenStr = '';
+        let oddStr = '';
+        for(let j = 0; j < stringArr[i].length; j++ ) {
+            (j % 2 === 0) ? evenStr += stringArr[i][j] : oddStr += stringArr[i][j];
+        }
+        console.log(`${evenStr} ${oddStr}`) 
+    }
 } 
 
 process.stdin.resume();
