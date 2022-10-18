591eb19710ea3ebb04b37da5a85bf04e4a027199
badshab454 <badshab454@gmail.com>
2022-10-18 23:12:13 +0600

    Dictionaries and Maps

--- Day 8/Dictionaries and Maps.js
+++ Day 8/Dictionaries and Maps.js
@@ -1,5 +1,25 @@
 function processData(input){
     //Enter your code here
+    function processData(input) {
+        const inputArr = input.split("\n");
+        const index = parseInt(inputArr[0]);
+        const phonebook = [];
+        
+    
+        for (let i = 0; i< index; i++){
+            let line = inputArr[i+1];
+            line = line.split(' ');
+            phonebook[line[0]] = line[1]
+        }
+        
+        for(let i = index+1; i < inputArr.length; i++) {
+            if(phonebook[inputArr[i]]) {
+                console.log(inputArr[i] + '=' + phonebook[inputArr[i]]);
+            } else {
+                console.log('Not found')
+            }
+        }
+    } 
 }
 
 processData.stdin.resume();
