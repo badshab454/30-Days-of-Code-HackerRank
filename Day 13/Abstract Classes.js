59b21788961bd2c2feb9e542f3b2c49671034886
badshab454 <badshab454@gmail.com>
2022-11-01 22:28:45 +0600

    Abstract Classes

--- Day 13/Abstract Classes.js
+++ Day 13/Abstract Classes.js
@@ -17,4 +17,25 @@
     // Write your method here
     
 // End class
-
+class MyBook extends Book {
+    
+    constructor(
+        title, author, price
+    ) {
+        super(
+            title,  author 
+        );
+        this.price = price;
+    }
+    
+    display() {
+        const obj = {
+            Title: this.title,
+            Author: this.author,
+            Price: this.price
+        }
+        for (const [key, value] of Object.entries(obj)) {
+            console.log(`${key}: ${value}`);
+        }
+    }
+}
