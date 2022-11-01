68e635530f86e340e113e35195cb0a700b3c34f7
badshab454 <badshab454@gmail.com>
2022-11-01 22:33:29 +0600

    Abstract Classes

--- Day 13/Abstract Classes.js
+++ Day 13/Abstract Classes.js
@@ -1,41 +1,12 @@
-
-// Declare your class here.
-    /**   
-    *   Class Constructor
-    *   
-    *   @param title The book's title.
-    *   @param author The book's author.
-    *   @param price The book's price.
-    **/
-    // Write your constructor here
-    
-    /**   
-    *   Method Name: display
-    *   
-    *   Print the title, author, and price in the specified format.
-    **/
-    // Write your method here
-    
-// End class
 class MyBook extends Book {
-    
-    constructor(
-        title, author, price
-    ) {
-        super(
-            title,  author 
-        );
-        this.price = price;
+    constructor(title, author, price){
+      super(title, author)
+      this.price = price
     }
     
-    display() {
-        const obj = {
-            Title: this.title,
-            Author: this.author,
-            Price: this.price
-        }
-        for (const [key, value] of Object.entries(obj)) {
-            console.log(`${key}: ${value}`);
-        }
+    display (){
+      console.log(`Title: ${this.title}`)
+      console.log(`Author: ${this.author}`)
+      console.log(`Price: ${this.price}`)
     }
 }
